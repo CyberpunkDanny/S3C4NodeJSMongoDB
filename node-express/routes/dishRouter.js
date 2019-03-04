32393d47e47919c9504c0812477305947e7778b8
Cyberpunk <divyatejadanny@gmail.com>
2019-03-04 17:40:05 +0530

    Ass-One: Node Modules, Express and REST API
    REST API End Points are created using 'route' for Dishes, Leaders & Promotions;

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -5,7 +5,6 @@ const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
-/* Inside index.js, dishRouter is mounted at '/dishes' endpoint*/
 dishRouter.route('/')
     .all((req, res, next) => {
         res.statusCode = 200;
@@ -24,6 +23,28 @@ dishRouter.route('/')
     })
     .delete((req, res, next)=>{
         res.end('Deleteing All Dishes');
+    });
+
+dishRouter.route('/:dishId')
+    .all((req, res, next) => {
+        console.log("\n\nReq: "+ req.url+ "\n\n");
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send details of the dish: '+ req.params.dishId+ ' to you!');
+    })
+    .post((req, res, next)=>{
+        res.statusCode = 403; //Forbidden
+        res.end('POST operation not supported on /dishes/'+req.params.dishId);
+    })
+    .put((req, res, next)=>{
+        res.write('Updating the dish: '+ req.params.dishId);
+        res.end('Will update the dish: ' + req.body.name + ' with details: '+ req.body.description);
+    })
+    .delete((req, res, next)=>{
+        res.end('Deleteing the dish: '+ req.params.dishId);
     });
 
 module.exports = dishRouter;
