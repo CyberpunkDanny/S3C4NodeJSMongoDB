fdac4ec9538ddfcc940058e8c855f7cc0ddaa5b4
Cyberpunk <divyatejadanny@gmail.com>
2019-03-12 19:21:19 +0530

    [D]:[GITHUB]Uploading Files using Multer NPM Module
    npm install multer --save;
    
    'uploadRouter' is created to support 'post'-ing of images (upload);
    
    Inside 'uploadRouter.js', 'multer' is configured with 'storage' & 'imageFileFilter';
    
    'storage' has 'destination' & 'filename' configured using 'multer.diskStorage()';
    
    [https://github/expressjs/multer/issues/722]
    
    'imageFileFilter' uses 'Regex' to match file extns;
    
    Then, 'upload', a multer module, is configured with these two;

---
+++ conFusionServer/routes/uploadRouter.js
@@ -0,0 +1,59 @@
+/* Supports uploading of files */
+const express = require('express');
+const bodyParser = require('body-parser');
+const authenticate = require('../authenticate');
+
+const multer = require('multer');
+
+/* 'multer' provides diskStorage() which enables us to define storage engine */
+const storage = multer.diskStorage({
+    destination: (req, file, callback)=>{
+        callback(null, 'public/images');
+    },
+    
+    filename: (req, file, callback)=>{
+        callback(null, file.originalname)
+    }
+});
+
+/* FileFilter enables us to specify which kind of files we are willing to accept for upload */
+const imageFileFilter = (req, file, callback)=>{
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+        return callback(new Error('You can upload only image files!'), false);
+    }
+    callback(null, true);
+};
+
+/* Configuring multer module */
+const upload = multer({storage: storage, fileFilter: imageFileFilter});
+
+const uploadRouter = express.Router();
+
+uploadRouter.use(bodyParser.json());
+
+uploadRouter.route('/')
+    .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+        res.statusCode = 403; //Forbidden
+        res.end('PUT operation not supported on /imageUpload');
+    })
+    
+    /* upload.single() takes as parameter the name of the form field which specifies that file */
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(req.file); /* req.file object contains the path to the file which can be used by client */
+    })
+    /* upload() will handle errors itself */
+    
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+        res.statusCode = 403; //Forbidden
+        res.end('PUT operation not supported on /imageUpload');
+    })
+
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+        res.statusCode = 403; //Forbidden
+        res.end('PUT operation not supported on /imageUpload');
+    })
+
+
+module.exports = uploadRouter;
